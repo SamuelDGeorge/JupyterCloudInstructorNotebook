cc7e8ae314ede7c8b97ec00dbf476904ca5f4e9e
Samuel George <sdgeorge@icloud.com>
2021-03-14 15:35:20 -0400

    Added Processing

--- DataTools/DataProcess.py
+++ DataTools/DataProcess.py
@@ -2,5 +2,21 @@ import pandasql as ps
 
 class DataProcess:
     
-    def __init__(self):
+    def __init__(self, event_frame):
+        self.event_frame = event_frame
+
+    def userActivity(self):
+        event_table = self.event_frame.copy()
+        sql = """
+            SELECT 
+                user as USER,
+                COUNT(activity) as USER_EVENTS
+            from event_table
+            GROUP BY user
+            ORDER BY USER_EVENTS DESC
+        """
+        out = ps.sqldf(sql, locals())
+        return out
+
+    def eventFrequency(self):
         pass
